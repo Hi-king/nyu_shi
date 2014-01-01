5810447c9c41258a015f9f5831f9b9fdec170df4
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2014-01-02 01:13:46 +0900

    択一問題が解けるようになる

--- bin/choice.py
+++ bin/choice.py
@@ -4,25 +4,42 @@ __author__ = 'hiking'
 __email__ = 'hikingko1@gmail.com'
 import os
 import sys
+import argparse
 script_path = os.path.dirname(__file__)
 script_path = script_path if len(script_path) else "."
-sys.path.append(script_path+"/../src/lib")
+sys.path.append(script_path+"/..")
+from lib import JapaneseVectorizer
+from lib import WikipediaScoreEvaluator
+
+def _argparser(rawarglist):
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('description', type=str)
+        parser.add_argument('options', type=str, nargs="+", default=[])
+        args = parser.parse_args(rawarglist)
+    except:
+        raise
+    return args
 
 def argmax(sequence):
     return sequence.index(max(sequence))
 
 def main(description, options):
     """
-    入力された４択から尤も回答である確率が高いものを出力する
+    入力された４択から最も回答である確率が高いものを出力する
     :param description: 問題文
     :type description: str
     :param options: 選択肢
     :type options: [str]
     """
-    description_token = Vectorizer.vectorize(description)
-    scores = [ScoreEvaluator.evaluate(vectorizer_token + Vectorizer.vectorize(option)) for option in options]
-    print scores
-    print argmax(scores)
+    description_token = JapaneseVectorizer.vectorize(description)
+    print description_token
 
+    scores = [WikipediaScoreEvaluator._evaluate(description_token + JapaneseVectorizer.vectorize(option)) for option in options]
+    print scores
+    ansindex = argmax(scores)
+    return options[ansindex]
 
 if __name__ == '__main__':
+    args = _argparser(sys.argv[1:])
+    main(args.description, args.options)
--- test/bin/test_choice.py
+++ test/bin/test_choice.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import unittest
 import choice
 class ChoiceTestCase(unittest.TestCase):
     def test_choice(self):
-        choice.main()
-
+        ans = choice.main("イギリスの首都", ["ロンドン", "ベルリン"])
+        self.assertEqual(ans, "ロンドン")
